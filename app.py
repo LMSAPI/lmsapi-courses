46807ff60ee9c37fbf31c46deb9b31cd677b7f3d
Seth Anderson <seth.c.anderson@gmail.com>
2017-04-30 19:17:48 -0500

    working courses

--- app.py
+++ app.py
@@ -29,6 +29,106 @@ def root():
     return 'hello there'
 
 
+@app.route('/courses/<course>/student', methods=['POST', 'GET', 'PUT', 'DELETE'])
+@require_appkey
+def student_courses(course):
+    mongo_student_courses = mongo.db.student_courses
+    teacheruser = user_name(request.args.get('key'))
+
+    # get all students in course
+    if request.method == 'GET':
+        courses_resp = mongo_student_courses.find({'number': course, 'teacheruser': teacheruser})
+        return json_util.dumps(courses_resp)
+
+    # insert new student into course
+    if request.method == 'POST':
+        existing_course = mongo_student_courses.find_one({'number': course,
+                                                          'email': request.args.get('email'),
+                                                          'teacheruser': teacheruser})
+        if existing_course is None:
+            mongo_student_courses.insert({'number': course,
+                                          'email': request.args.get('email'),
+                                          'enrolled': request.args.get('enrolled'),
+                                          'teacheruser': teacheruser})
+            return 'Added'
+
+        return 'already exists!'
+
+    # update a student in a course
+    if request.method == 'PUT':
+        existing_course = mongo_student_courses.find_one({'number': course,
+                                                          'email': request.args.get('email'),
+                                                          'teacheruser': teacheruser})
+        if existing_course is not None:
+            print(request.args)
+            mongo_student_courses.update_one({'number': course,
+                                              'email': request.args.get('email'),
+                                              'teacheruser': teacheruser},
+                                             {'$set': {'enrolled': request.args.get('enrolled')}})
+            return 'Updated'
+
+        return 'Update failed'
+
+    # delete a student from a course
+    if request.method == 'DELETE':
+        existing_course = mongo_student_courses.find_one({'number': course,
+                                                          'email': request.args.get('email'),
+                                                          'teacheruser': teacheruser})
+        if existing_course is not None:
+            mongo_student_courses.delete_one({'number': course,
+                                              'email': request.args.get('email'),
+                                              'teacheruser': teacheruser})
+            return 'Deleted'
+
+        return 'Delete failed'
+
+
+@app.route('/courses', methods=['POST', 'GET'], defaults={'course': None})
+@app.route('/courses/<course>', methods=['PUT', 'DELETE'])
+@require_appkey
+def courses(course):
+    mongo_courses = mongo.db.courses
+    teacheruser = user_name(request.args.get('key'))
+
+    # get all courses
+    if request.method == 'GET':
+        courses_resp = mongo_courses.find({'teacheruser': teacheruser})
+        return json_util.dumps(courses_resp)
+
+    # insert new course
+    if request.method == 'POST':
+        existing_course = mongo_courses.find_one({'number': request.args.get('number'), 'teacheruser': teacheruser})
+        if existing_course is None:
+            mongo_courses.insert({'number': request.args.get('number'),
+                                  'title': request.args.get('title'),
+                                  'prereq': request.args.get('prereq'),
+                                  'teacheruser': teacheruser})
+            return 'Added'
+
+        return 'already exists!'
+
+    # update a course
+    if request.method == 'PUT':
+        existing_course = mongo_courses.find_one({'number': course, 'teacheruser': teacheruser})
+        if existing_course is not None:
+            print(request.args)
+            mongo_courses.update_one({'number': course, 'teacheruser': teacheruser},
+                                     {'$set': {'title': request.args.get('title'),
+                                               'prereq': request.args.get('prereq')}})
+            return 'Updated'
+
+        return 'Update failed'
+
+    # delete a course
+    if request.method == 'DELETE':
+        existing_course = mongo_courses.find_one({'number': course, 'teacheruser': teacheruser})
+        if existing_course is not None:
+            mongo_courses.delete_one({'number': course, 'teacheruser': teacheruser})
+            return 'Deleted'
+
+        return 'Delete failed'
+
+
 def obj_dict(obj):
     return obj.__dict__
 
